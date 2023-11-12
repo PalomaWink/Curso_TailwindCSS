47c775f79d749305ecc5e86d0db6c12923e78fb8
Paloma Wink <paloma.wink@hotmail.com>
2023-11-12 20:43:50 -0300

    Aprendendo a trabalhar com a config do tailwind

--- fundamentos_tailwind/tailwind.config.js
+++ fundamentos_tailwind/tailwind.config.js
@@ -1,8 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
+//const { colors } = require('tailwindcss/defaultTheme')
+
+/* A partir do momento que coloca uma regra dentro do theme, sobrescreve tudo
+ou seja, é como se agora só tivese a cor black, todas as outras cores foram apagadas */
+
+/* Consigo tambem criar breakpoints, diferente do que o Tailwind nos tras lg, md, etc..
+dentro de theme usando a chave screens, mas lembrendo que ao fazer isso, o resto todo é perdido */
+
+/* Para colocar configurações a mais usamos a chave extend */
+
+/* Dentro do extend eu consigo mudar as cores padrão do tailwind, mas sem mudar tudo, na linha 27 eu to
+mudando o gray-900 do tailwind, mantendo todas as outras */
+
+/* Alterando o espaçamento
+na chave spacing podemos alterar o espaçamento do projeto, podemos definir nossos próprios ou utilizar
+o extend para adicionar mais espaçamentos no padrão do Tailwind por rem, por exemplo
+ele da uma escala de tamanho para utilizarmos em algumas classes que utilizam números, ex: p- m-*/
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    /* colors: {
+      'black': '#010101',
+    }, */
+    /* screens: {
+      'tablet': '640px',
+    }, */
+    extend: {
+      colors: {
+        'new-blue': '#243C5A',
+        /* gray: {
+          ...colors.gray,
+          '900': '#999'
+        }, */
+      },
+      spacing: {
+        '50': '20rem',
+      }
+    },
   },
   plugins: [],
 }
